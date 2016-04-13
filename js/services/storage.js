64822ddebc5b7ed460706badb5cbc4c2cf7f8251
slnpacifist <slnpacifist@gmail.com>
2016-04-13 16:05:42 +0800

    Changed storage to localStorage

--- js/services/storage.js
+++ js/services/storage.js
@@ -6,22 +6,15 @@ angular.module('webui.services.storage', ['webui.services.utils'])
   }
 
   function get(key) {
-    var chunks = document.cookie.split(";");
-    for (var i = 0; i < chunks.length; i++) {
-      var ckey = chunks[i].substr(0, chunks[i].indexOf("=")).replace(/^\s+|\s+$/g,"");
-      var cvalue = chunks[i].substr(chunks[i].indexOf("=") + 1);
-      if (key == ckey) {
-        return JSON.parse(unescape(cvalue));
-      }
+    console.log("Reading", key);
+    if (key in localStorage) {
+      return JSON.parse(localStorage[key]);
+    } else {
+      return null;
     }
-
-    return null;
   }
 
   function set(key, value) {
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + 30 * 12);
-    var cvalue = escape(JSON.stringify(value)) + "; expires=" + exdate.toUTCString();
-    document.cookie = key + "=" + cvalue;
+    localStorage[key] = JSON.stringify(value);
   }
 }]);
